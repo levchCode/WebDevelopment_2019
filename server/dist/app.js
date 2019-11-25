c7e47a4505e8e43703fc73fb5b9ccaae1e5a99b1
BSOD_levch <levch98@mail.ru>
2019-11-25 16:53:14 +0300

    Параметры запросов заработали

--- server/dist/app.js
+++ server/dist/app.js
@@ -10,20 +10,26 @@ const app = express();
 const port = 9000;
 const MongoClient = mg.MongoClient;
 const uri = config_json_1.default.url;
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Animatr backend, nothing to see here, folks!');
 });
-app.get('/test', (req, res) => {
+// Профиль: должен вернуть имя пользователя, общее кол-во просмотров и лайков и список видео пользователя по user_id
+app.post('/profile', (req, res) => {
+    console.log(req.body);
     MongoClient.connect(uri, (err, client) => {
         const collection = client.db('anim').collection('users');
         collection.find().toArray()
             .then((data) => {
-            console.log(data);
             res.send({ d: data });
             client.close();
         });
     });
 });
+// Просмотр видео: возращает информацию и само видео, по video_id
+// Загрузка видео: загружает информацию и само видео и возращает video_id
+// Популярное: возращает 10 видео по убыванию просмотров
+// Засчитать просмотр или лайк
 // start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
--- server/src/app.ts
+++ server/src/app.ts
@@ -8,11 +8,16 @@ const port = 9000;
 const MongoClient = mg.MongoClient;
 const uri = config.url;
 
+app.use(express.json());
+
 app.get('/', (req: any, res: any) => {
     res.send('Animatr backend, nothing to see here, folks!');
 });
 
-app.get('/profile', (req: any, res: any) => {
+// Профиль: должен вернуть имя пользователя, общее кол-во просмотров и лайков и список видео пользователя по user_id
+app.post('/profile', (req: any, res: any) => {
+
+    let user_id = req.body;
 
     MongoClient.connect(uri, (err: any, client: any) => {
 
@@ -24,6 +29,11 @@ app.get('/profile', (req: any, res: any) => {
         });
       });
 });
+
+// Просмотр видео: возращает информацию и само видео, по video_id
+// Загрузка видео: загружает информацию и само видео и возращает video_id
+// Популярное: возращает 10 видео по убыванию просмотров
+// Засчитать просмотр или лайк
 
 // start the Express server
 app.listen(port, () => {
