4d78f0bfc839510b2b564453093bf59900a55c22
BSOD_levch <levch98@mail.ru>
2019-11-26 18:03:21 +0300

    [Сервер] Запросы, доделать запросы profile

--- server/dist/app.js
+++ server/dist/app.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const mg = require("mongodb");
+const mongodb_1 = require("mongodb");
 const config_json_1 = __importDefault(require("./config.json"));
 const app = express();
 const port = 9000;
@@ -15,21 +16,80 @@ app.get('/', (req, res) => {
     res.send('Animatr backend, nothing to see here, folks!');
 });
 // Профиль: должен вернуть имя пользователя, общее кол-во просмотров и лайков и список видео пользователя по user_id
-app.post('/profile', (req, res) => {
-    const user_id = req.body;
+app.get('/profile', (req, res) => {
+    const u_id = req.body.user_id;
     MongoClient.connect(uri, (err, client) => {
-        const collection = client.db('anim').collection('users');
-        collection.find().toArray()
-            .then((data) => {
-            res.send({ d: data });
+        const collection = client.db('anim').collection('videos');
+        collection.aggregate([
+            { $match: { user_id: mongodb_1.ObjectId(u_id) } },
+            { $group: { _id: '$user_id', total_views: { $sum: '$views' }, total_likes: { $sum: '$likes' } } },
+        ]).toArray().then((data) => {
+            res.send(data);
             client.close();
         });
     });
 });
-// Просмотр видео: возращает информацию и само видео, по video_id
-// Загрузка видео: загружает информацию и само видео и возращает video_id
-// Популярное: возращает 10 видео по убыванию просмотров
-// Засчитать просмотр или лайк
+// {+} Просмотр видео: возращает информацию и само видео, по video_id
+app.get('/watch', (req, res) => {
+    const video_id = req.body.video_id;
+    MongoClient.connect(uri, (err, client) => {
+        const collection = client.db('anim').collection('videos');
+        collection.find({ _id: mongodb_1.ObjectId(video_id) }).toArray()
+            .then((data) => {
+            res.send(data);
+            client.close();
+        });
+    });
+});
+// {+} Загрузка видео: загружает информацию и само видео и возращает video_id
+app.post('/upload', (req, res) => {
+    const video_payload = req.body;
+    MongoClient.connect(uri, (err, client) => {
+        const collection = client.db('anim').collection('videos');
+        collection.insertOne(video_payload).then((result) => {
+            res.send({ video_id: result.insertedId });
+            client.close();
+        });
+    });
+});
+// {+} Популярное: возращает 10 видео по убыванию просмотров
+app.get('/popular', (req, res) => {
+    MongoClient.connect(uri, (err, client) => {
+        const collection = client.db('anim').collection('videos');
+        collection.find().sort({ views: -1 }).limit(10).toArray()
+            .then((data) => {
+            res.send(data);
+            client.close();
+        });
+    });
+});
+// {+} Засчитать просмотр или лайк
+app.post('/viewlike', (req, res) => {
+    const data = req.body;
+    MongoClient.connect(uri, (err, client) => {
+        const collection = client.db('anim').collection('videos');
+        if (data.action === 'view') {
+            collection.updateOne({
+                _id: mongodb_1.ObjectId(data.video_id),
+            }, {
+                $inc: { views: 1 },
+            }).then(() => {
+                res.send({ result: 1 });
+                client.close();
+            });
+        }
+        else {
+            collection.updateOne({
+                _id: mongodb_1.ObjectId(data.video_id),
+            }, {
+                $inc: { likes: 1 },
+            }).then(() => {
+                res.send({ result: 1 });
+                client.close();
+            });
+        }
+    });
+});
 // start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express = require('express');
 import mg = require('mongodb');
+import { ObjectId } from 'mongodb';
 import config from './config.json';
 
 const app = express();
@@ -15,82 +16,105 @@ app.get('/', (req: any, res: any) => {
 });
 
 // Профиль: должен вернуть имя пользователя, общее кол-во просмотров и лайков и список видео пользователя по user_id
-app.post('/profile', (req: any, res: any) => {
+app.get('/profile', (req: any, res: any) => {
 
-    const user_id = req.body;
-
-    MongoClient.connect(uri, (err: any, client: any) => {
-
-        const collection = client.db('anim').collection('users');
-        collection.find().toArray()
-        .then((data: any) => {
-            res.send({d: data});
-            client.close();
-        });
-      });
-});
-
-// Просмотр видео: возращает информацию и само видео, по video_id
-app.get('/watch', (req: any, res: any) => {
-
-    const video_id = req.body;
+    const u_id = req.body.user_id;
 
     MongoClient.connect(uri, (err: any, client: any) => {
 
         const collection = client.db('anim').collection('videos');
-        collection.find().toArray()
+        collection.aggregate([
+                { $match: { user_id: ObjectId(u_id) }},
+                { $group: { _id: '$user_id', total_views: { $sum: '$views' }, total_likes: { $sum: '$likes' }}},
+        ]).toArray().then((data: any) => {
+            res.send(data);
+            client.close();
+        });
+    });
+});
+
+// {+} Просмотр видео: возращает информацию и само видео, по video_id
+app.get('/watch', (req: any, res: any) => {
+
+    const video_id = req.body.video_id;
+
+    MongoClient.connect(uri, (err: any, client: any) => {
+
+        const collection = client.db('anim').collection('videos');
+        collection.find({_id: ObjectId(video_id)}).toArray()
         .then((data: any) => {
-            res.send({d: data});
+            res.send(data);
             client.close();
         });
       });
 });
 
-// Загрузка видео: загружает информацию и само видео и возращает video_id
+// {+} Загрузка видео: загружает информацию и само видео и возращает video_id
 app.post('/upload', (req: any, res: any) => {
 
-    const video = req.body;
+    const video_payload = req.body;
 
     MongoClient.connect(uri, (err: any, client: any) => {
-
-        const collection = client.db('anim').collection('users');
-        collection.find().toArray()
-        .then((data: any) => {
-            res.send({d: data});
+        const collection = client.db('anim').collection('videos');
+        collection.insertOne(video_payload).then((result: any) => {
+            res.send({video_id: result.insertedId});
             client.close();
         });
-      });
+
+    });
 });
 
-// Популярное: возращает 10 видео по убыванию просмотров
+// {+} Популярное: возращает 10 видео по убыванию просмотров
 app.get('/popular', (req: any, res: any) => {
 
-
     MongoClient.connect(uri, (err: any, client: any) => {
 
-        const collection = client.db('anim').collection('users');
-        collection.find().toArray()
+        const collection = client.db('anim').collection('videos');
+        collection.find().sort({views: -1}).limit(10).toArray()
         .then((data: any) => {
-            res.send({d: data});
+            res.send(data);
             client.close();
         });
       });
 });
 
-// Засчитать просмотр или лайк
+// {+} Засчитать просмотр или лайк
 app.post('/viewlike', (req: any, res: any) => {
 
-    const video_id = req.body;
+    const data = req.body;
 
     MongoClient.connect(uri, (err: any, client: any) => {
 
-        const collection = client.db('anim').collection('users');
-        collection.find().toArray()
-        .then((data: any) => {
-            res.send({d: data});
-            client.close();
-        });
-      });
+        const collection = client.db('anim').collection('videos');
+        if (data.action === 'view') {
+
+            collection.updateOne(
+                {
+                    _id: ObjectId(data.video_id),
+                },
+                {
+                    $inc: { views: 1 },
+                },
+            ).then(() => {
+                res.send({result: 1});
+                client.close();
+            });
+        } else {
+            collection.updateOne(
+                {
+                    _id: ObjectId(data.video_id),
+                },
+                {
+                    $inc: { likes: 1 },
+                },
+            ).then(() => {
+                res.send({result: 1});
+                client.close();
+            });
+        }
+
+    });
+
 });
 
 // start the Express server
